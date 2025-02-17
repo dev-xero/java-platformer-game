3fa74a7131a3fbcc123b5bad9233ed33e5a5e5c6
= <ssh.xero@gmail.com>
2025-02-17 23:22:05 +0100

    feat: Implement much faster game loop with threads

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,12 +1,56 @@
 package main;
 
-public class Game {
+public class Game implements Runnable {
+
+    private final GamePanel gamePanel;
+    private final GameWindow gameWindow;
+
+    private Thread gameLoopThread;
+    private final int FPS_SET = 120;
 
     public Game() {
-        GamePanel gamePanel = new GamePanel();
-        GameWindow gameWindow = new GameWindow(gamePanel);
-
+        this.gamePanel = new GamePanel();
+        this.gameWindow = new GameWindow(gamePanel);
         gamePanel.requestFocus();
+        startGameLoop();
+    }
+
+    // Separate thread for updating the game loop
+    private void startGameLoop() {
+        this.gameLoopThread = new Thread(this);
+        this.gameLoopThread.start();
+    }
+
+    @Override
+    public void run() {
+        double timePerFrame = 1_000_000_000.0 / FPS_SET;
+        long lastFrameTime = System.nanoTime();
+        long lastFpsCheck = System.currentTimeMillis();
+        int frames = 0;
+
+        while (true) {
+            long now = System.nanoTime();
+
+            if (now - lastFrameTime >= timePerFrame) {
+                gamePanel.updateRectangle();
+                gamePanel.repaint();
+                lastFrameTime = now;
+                frames++;
+            }
+
+            // Print FPS every second
+            if (System.currentTimeMillis() - lastFpsCheck >= 1000) {
+                System.out.println("FPS: " + frames);
+                frames = 0;
+                lastFpsCheck = System.currentTimeMillis();
+            }
+
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -9,17 +9,10 @@ import java.util.Random;
 
 public class GamePanel extends JPanel {
 
-    // Positioning and direction
     private float xDelta = 100, yDelta = 100;
-    private float xDirection = 0.003f, yDirection = 0.003f;
-
-    // Random colorization
+    private float xDirection = 1.0f, yDirection = 1.0f;
     private final Random random;
     private Color color = new Color(150, 20, 90);
-
-    // Frame rate
-    private int frames = 0;
-    private long lastFrameCheck = 0;
 
     // Instantiates new game panel
     public GamePanel() {
@@ -35,18 +28,18 @@ public class GamePanel extends JPanel {
 
     // Increments x position by dx
     public void changeXDelta(int dx) {
-        this.xDelta += dx;
+        xDelta += dx;
     }
 
     // Increments y position by dy
     public void changeYDelta(int dy) {
-        this.yDelta += dy;
+        yDelta += dy;
     }
 
     // Sets rectangle position to (x, y) coordinates
     public void setRectPosition(int x, int y) {
-        this.xDelta = x;
-        this.yDelta = y;
+        xDelta = x;
+        yDelta = y;
     }
 
     // Generates a random hex triplet color
@@ -60,39 +53,26 @@ public class GamePanel extends JPanel {
 
     // Updates rectangle direction and colors
     public void updateRectangle() {
-        this.xDelta += xDirection;
-        this.yDelta += yDirection;
-
-        if (0 > this.xDelta || this.xDelta > 400) {
+        xDelta += xDirection;
+        if (0 > xDelta || xDelta > 400) {
             xDirection *= -1;
             color = getRandomColor();
         }
 
-        if (0 > this.yDelta || this.yDelta > 400) {
+        yDelta += yDirection;
+        if (0 > yDelta || yDelta > 400) {
             yDirection *= -1;
             color = getRandomColor();
         }
     }
 
     // Paint component is called everytime the ui updates
+    @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        updateRectangle();
-
         g.setColor(color);
-        g.fillRect((int)xDelta,  (int)yDelta, 200, 50);
-
-        // Calculate frames per second
-        frames++;
-        if (System.currentTimeMillis() - lastFrameCheck >= 1000) {
-            lastFrameCheck = System.currentTimeMillis();
-            System.out.println("FPS: " + frames);
-
-            frames = 0;
-        }
-
-        repaint();
+        g.fillRect((int) xDelta,  (int) yDelta, 200, 50);
     }
 
 }
