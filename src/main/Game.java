e90f248eda057da02796d2e03f9f14854b435ae5
= <ssh.xero@gmail.com>
2025-02-18 03:17:02 +0100

    refactor: Improve code readability

--- src/main/Game.java
+++ src/main/Game.java
@@ -8,35 +8,39 @@ public class Game implements Runnable {
     private Thread gameLoopThread;
     private final int FPS_SET = 120;
 
-    // Instantiates a new class
+
     public Game() {
-        this.gamePanel = new GamePanel();
-        this.gameWindow = new GameWindow(gamePanel);
+
+        gamePanel = new GamePanel();
+        gameWindow = new GameWindow(gamePanel);
+
         gamePanel.requestFocus();
+
         startGameLoop();
+
     }
 
-    // Separate thread for updating the game loop
     private void startGameLoop() {
-        this.gameLoopThread = new Thread(this);
-        this.gameLoopThread.start();
+        gameLoopThread = new Thread(this);
+        gameLoopThread.start();
     }
 
-    // This task is performed in the game loop thread
     @Override
     public void run() {
         double timePerFrame = 1_000_000_000.0 / FPS_SET;
+
         long lastFrameTime = System.nanoTime();
         long lastFpsCheck = System.currentTimeMillis();
+
         int frames = 0;
 
         while (true) {
             long now = System.nanoTime();
 
-            // Repaint UI after time per frame
             if (now - lastFrameTime >= timePerFrame) {
                 gamePanel.repaint();
                 lastFrameTime = now;
+
                 frames++;
             }
 
@@ -46,13 +50,6 @@ public class Game implements Runnable {
                 frames = 0;
                 lastFpsCheck = System.currentTimeMillis();
             }
-
-            // Prevent overworking the CPU :(
-//            try {
-//                Thread.sleep(1);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
         }
     }
 
