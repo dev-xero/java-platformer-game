6aaa83b7440ebcb200eeafd7eec1d0c0d4d864d4
= <ssh.xero@gmail.com>
2025-02-21 23:33:59 +0100

    fix: Draw level behind player

--- src/main/Game.java
+++ src/main/Game.java
@@ -17,7 +17,7 @@ public class Game implements Runnable {
     private Player player;
     private LevelManager levelManager;
 
-    public final static float SCALE = 1.5f;
+    public final static float SCALE = 2.0f;
     private final static int TILE_DEFAULT_SIZE = 32;
     public final static int TILES_IN_WIDTH = 26;
     public final static int TILES_IN_HEIGHT = 14;
@@ -44,8 +44,8 @@ public class Game implements Runnable {
 
     /** Synchronizes game  entities to render. */
     public void render(Graphics g) {
-        player.render(g);
         levelManager.render(g);
+        player.render(g);
     }
 
     /** Returns instantiated player object. */
